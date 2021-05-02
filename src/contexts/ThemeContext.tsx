8ca2ff7725bf0d5cd2199e79b5d87595536a80f9
unknown <piccini.marina21@gmail.com>
2021-05-02 14:29:49 -0300

    first commit

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { ThemeProvider as SCThemeProvider } from 'styled-components'
+import { light, dark } from '../components/Pancakeswap/uikit/src'
+
+const CACHE_KEY = 'IS_DARK'
+
+const ThemeContext = React.createContext({ isDark: null, toggleTheme: () => null })
+
+const ThemeContextProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(() => {
+    const isDarkUserSetting = localStorage.getItem(CACHE_KEY)
+    return isDarkUserSetting ? JSON.parse(isDarkUserSetting) : true
+  })
+
+  const toggleTheme = () => {
+    setIsDark((prevState) => {
+      localStorage.setItem(CACHE_KEY, JSON.stringify(!prevState))
+      return !prevState
+    })
+  }
+
+  const tempDark = { ...dark, tooltip: { ...dark.tooltip, background: "#ff8800" }, colors: { ...dark.colors, primary: "#0056AC", textSubtle: "#ff8800", backgroundAlt: "#000", secondary: "#ff8800", text: "#ff8800", gradients: {...dark.colors.gradients, bubblegum: 'linear-gradient(139.73deg,#313D5C 0%,#201e21 100%)'} }, card: { ...dark.card, background: "#000" } }
+
+  return (
+    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+      <SCThemeProvider theme={isDark ? tempDark : light}>{children}</SCThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+export { ThemeContext, ThemeContextProvider }
